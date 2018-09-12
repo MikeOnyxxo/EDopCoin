7e641ecb8053342efc1edfe0765233cc64da4ed6
MikeOnyxxo <41545096+MikeOnyxxo@users.noreply.github.com>
2018-09-12 19:14:45 -0400

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 
 const char     CRYPTONOTE_NAME[]                             = "edopcoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff000101029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101663d57bb8b2d432df8fb67cc3b2bda23e40a4767f7dc631fc603a0fc75308853";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
